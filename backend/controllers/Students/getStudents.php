c73f9d39e4b8dae301daa2a67399e1c16be182bc
Miltonus <lawrence.gabriel624@gmail.com>
2025-11-25 00:28:01 +0800

    Changed some codes regarding to Student Controller

--- backend/controllers/Students/getStudents.php
+++ backend/controllers/Students/getStudents.php
@@ -1,10 +1,17 @@
 <?php
     header('Content-Type: application/json');
 
+    if($_SERVER['REQUEST_METHOD'] !== 'GET'){
+        http_response_code(405);
+        echo json_encode([
+            "message" => "Method Not Allowed"
+        ]);
+        exit();
+    }
     $student_id = $_GET['stud_id'] ?? null;
 
-    if($userId){
-        require_once('../conn.php');
+    if($student_id){
+        require_once('../../db/conn.php');
         //sql
         $sql = "SELECT * FROM tbl_students WHERE id = :id";
 
@@ -32,20 +39,20 @@
             exit();
         }
     } else {
-        require_once('../conn.php');
+        require_once('../../db/conn.php');
         //sql
-        $sql = "SELECT * FROM items";
+        $sql = "SELECT * FROM tbl_students";
         //prep
         $stmt = $conn->prepare($sql);
         //exe
         $stmt->execute();
         //fetch
-        $items = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        $students = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
         //echo
         echo json_encode([
             "success" => true,
-            "items" => $items
+            "Students" => $students
         ]);
     }
 ?>
