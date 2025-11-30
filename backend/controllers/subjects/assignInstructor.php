9d32be5fbc22aa9a72cbd0a56e9fd20779d3f8e4
Miltonus <lawrence.gabriel624@gmail.com>
2025-11-30 13:02:24 +0800

    added assign instructor feature to subject

--- backend/controllers/subjects/assignInstructor.php
+++ backend/controllers/subjects/assignInstructor.php
@@ -11,16 +11,34 @@
     }
 
     //get update input
-    $selected_class = $_POST['class_id'];
-    $selected_instructor = $_POST['instructor_id'];
+    $input = json_decode(file_get_contents("php://input"),true);
+    $subject_id = $input['subject_id'] ?? null;
+    $instructor_id = $input['instructor_id'] ??  null;
 
     require_once('../../db/conn.php');
     try{
-        
+        if(!$subject_id || !$instructor_id){
+            http_response_code(406);
+            echo json_encode([
+                "message" => "Incomplete Fields"
+            ]);
+            exit();
+        }
+        $sql = "UPDATE tbl_instructors SET subject_id = :subject_id  WHERE instructor_id = :instructor_id";
+        $stmt = $conn -> prepare($sql);
+        $stmt ->bindParam(":subject_id", $subject_id);
+        $stmt ->bindParam(":instructor_id", $instructor_id);
+        $stmt -> execute();
+
+        http_response_code(200);
+        echo json_encode([
+            "message" => "Teacher Assigned Succesfully"
+        ]);
+        exit();
     } catch (PDOException $e) {
-
+        http_response_code(500);
+        echo json_encode([
+            "message" => "Error" . $e ->getMessage()
+        ]);
     }
-
-
-
 ?>
